fcdaab2db56dfe253eebf98ef2c7de8c76ee400e
Joe Spandrusyszyn <github@illandril.net>
2018-09-27 12:43:18 -0400

    Update joe.js

--- joe.js
+++ joe.js
@@ -4,6 +4,6 @@ people.push({
  color: 'blueGreen',
  link: 'joe.html',
  lived: ['ny', 'fl'],
- visited: ['ct', 'ga', 'in', 'ky', 'ma', 'md', 'mi', 'nj', 'oh', 'pa', 'sc', 'va', 'vt', 'me', 'nv', 'ut', 'az', 'wa', 'nc'],
- passedThru: ['de', 'nh', 'wv']
+ visited: ['ct', 'ga', 'in', 'ky', 'ma', 'md', 'mi', 'nj', 'oh', 'pa', 'sc', 'va', 'vt', 'me', 'nv', 'ut', 'az', 'wa', 'nc', 'wv'],
+ passedThru: ['de', 'nh']
 });
